6cc475110516b8c624947827516b06e347978ee3
Dmytro Kasianchyk <dikape03@gmail.com>
2017-11-06 18:37:36 +0200

    added user

--- socket_app/chat/socket_handlers.py
+++ socket_app/chat/socket_handlers.py
@@ -1,42 +1,45 @@
 import socketio
+import datetime
 
-mgr = socketio.AsyncRedisManager('redis://')
+mgr = socketio.AsyncRedisManager('redis://127.0.0.1:6379')
 sio = socketio.AsyncServer(client_manager=mgr)
 
 
-__all__ = ('connect',
-           'message',
-           'disconnect')
+class ChatNamespace(socketio.AsyncNamespace):
+    def __init__(self, *args, **kwargs):
+        self.users_sid = {}
+        super().__init__(*args, **kwargs)
+
+    async def on_connect(self, sid, environ):
+        request = environ.get('aiohttp.request')
+        room = environ['HTTP_REFERER'].split('/')[-1]
+        if request.user:
+            self.users_sid[sid] = request.user
+            self.enter_room(sid, room=room)
+            await self.emit('online', {'user': request.user.username}, room=room)
+
+    async def on_message(self, sid, data):
+        message = {
+            'text': data['message'],
+            'user': self.users_sid[sid].username,
+            # 'time': datetime.datetime.now().time()
+
+        }
+        await self.emit('reply', message, room=data['room'])
+
+    # def on_disconnect(self, sid):
+    #     print('disconnect ', sid)
+
+    # def on_enter_room(self, sid, data):
+    #     if data['room'] and sid:
+    #         print(data['room'] + '----room')
+    #         # # print(sio)
+    #         # print("User has entered to room {0}".format(data['room']))
 
 
-@sio.on('connect', namespace='/chat')
-async def connect(sid, environ):
-    request = environ.get('aiohttp.request')
-    if request.user:
-        await sio.emit('online', {'user': request.user.username}, namespace='/chat')
+    # def on_close_room(self, room):
+    #     # print("Room {0} has been deleted".format(room))
+    #     self.close_room(room=room)
 
 
-@sio.on('message', namespace='/chat')
-async def message(sid, data):
-    print("message ", data)
-    await sio.emit('reply', data['message'], namespace='/chat', room=data['room'])
-
-
-@sio.on('disconnect', namespace='/chat')
-async def disconnect(sid):
-    print('disconnect ', sid)
-
-
-@sio.on('close_room', namespace='/chat')
-def close_room(room):
-    print("Room {0} has been deleted".format(room))
-    sio.close_room(room=room, namespace='/chat')
-
-
-@sio.on('enter_room', namespace='/chat')
-def enter_room(sid, data):
-    if data['room'] and sid:
-        print(sio)
-        print("User has entered to room {0}".format(data['room']))
-        sio.enter_room(sid, room=data['room'], namespace='/chat')
-
+sio.register_namespace(ChatNamespace('/chat'))
